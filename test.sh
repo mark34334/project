762760f697b7a8cf7cedaf5aaf0f5076488508bb
mark34334 <116044638+mark34334@users.noreply.github.com>
2023-02-23 21:31:56 +0530

    Updated the gobuster into test.sh
    gobuster will have extra point to discover the area of web directory

--- test.sh
+++ test.sh
@@ -5,19 +5,26 @@ if ! command -v dirb &> /dev/null; then
   echo "Error: dirb is not installed. Please install it before running this script."
   exit 1
 fi
-:'
-# Check if dirbuster is installed
-if ! command -v dirbuster &> /dev/null; then
-  echo "Error: dirbuster is not installed. Please install it before running this script."
-  exit 1
+
+#------------------------------------------------------------------------------------------------ 
+#Check if the gobuster is installed in the system
+
+if ! command -v gobuster &> /dev/null
+then
+    echo "Gobuster is not installed on the system. Please install Gobuster and try again."
+    exit 1
 fi
-'
+#------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------
 # Check if nmap is installed
+
+
 if ! [ -x "$(command -v nmap)" ]; then
   echo "Error: nmap is not installed. Please install nmap and try again." >&2
   exit 1
 fi
-
+#------------------------------------------------------------------------------------------------
 # Check if target website is up and running
 function check_status {
   status=$(curl -s -o /dev/null -w '%{http_code}' $1)
@@ -27,7 +34,7 @@ function check_status {
     echo "Website is down"
   fi
 }
-
+#------------------------------------------------------------------------------------------------
 # Extract server information
 function server_info {
   server=$(curl -sI $1)
@@ -47,7 +54,7 @@ function nmap_scan {
 }
 
 #Use dirb to scan for the directory # Create the results directory if it doesn't exist
-function dirb_scan {
+function dirb_scan { 
 #making a directory
   RESULTS_DIR="./dirb_results"
   if [ ! -d "$RESULTS_DIR" ]; then
@@ -55,32 +62,30 @@ function dirb_scan {
   fi
   dirb "$1" -o "$RESULTS_DIR/dirb_results.txt"
 }
-:'
-#Use dirbuster to scan for the directory from the websites
-function dirbuster_scan {
-# Set the wordlist to use for the scan
-WORDLIST="/usr/share/wordlists/dirb/common.txt"
-
-# Set the directory to save the scan results
-RESULTS_DIR="./dirbuster_results"
-
-# Create the results directory if it doesnt exist
-if [ ! -d "$RESULTS_DIR" ]; then
-  mkdir -p "$RESULTS_DIR"
-fi	
-'
-# Run the dirbuster scan
-dirbuster -u "$1" -l "$WORDLIST" -o "$RESULTS_DIR/dirbuster_results.html" -F
+#-----------------------------------------------------------------------------------------------------
+#Use the gobuster to scan for the direcotory
+function gobuster_scan {
+	read -p "Enter your name: " NAME
+  echo "Hello, $NAME! Nice to meet you."
+  	read -p "So let's start the attack can you tell me threads cound: " thread
+  	echo "Okay, thread count pointed at $thread"
+	mkdir -p gobuster_results
+	wordlist=/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt
+# Run Gobuster and store the output in a file
+	echo "Starting the attack on $1 domain."
+	gobuster dir -u "$1" -w "$wordlist" -t "$thread" -o gobuster_results/output.txt
+	echo "Gobuster scan completed. Results saved in gobuster_results directory."
 }
 
+#-----------------------------------------------------------------------------------------------------
 # Main function
 function main {
   check_status $1
   server_info $1
   ip_address $1
   nmap_scan $1
-  dirb_scan $1
-  #dirbuster_scan $1 
+  dirb_scan $1 
+  gobuster_scan $1
 }
 
 # Menu
@@ -92,7 +97,7 @@ while true; do
   echo "4. nmap scan"
   echo "5. Full reconnaissance"
   echo "6. Directory search using dirb"
-  echo "7. Directory search using dirbuster"
+  echo "7. Directory search using gobuster"
   echo "8. Exit"
   read -p "Choose an option [1-8]: " option
 
@@ -123,7 +128,7 @@ while true; do
       ;;
     7)
       read -p "Enter target website: " website
-      dirbuster_scan $website 
+      gobuster_scan $website 
       ;;
     8)
       break
